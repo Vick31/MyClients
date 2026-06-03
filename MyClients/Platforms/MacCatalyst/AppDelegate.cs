f8888507b7599cdc1707e54ba3362c61bd3b969e
Manuel Perez <89532383+Vick31@users.noreply.github.com>
2026-06-03 09:58:22 -0500

    create proyect

---
+++ MyClients/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MyClients
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
